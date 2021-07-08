044da7cf6b0c34993f7a732ed82d1436ec8c6341
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-07-07 17:57:47 -0700

    add styling and layout to dev work list page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89bb2fd62632f1633013"; }
+/******/ 		__webpack_require__.h = function() { return "05dafe87d1d21f846419"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/DevWork.js
+++ components/DevWork.js
@@ -1,36 +1,36 @@
+import styles from '../styles/workList.module.css'
+
 export default function DevWork({ setPage }) {
+
     return (
         // <p>examples of my design work coming soon</p>
         <div>
-            <div>
-                <h1>GIM LLC.</h1>
-                <img src='/GIM.png' alt='screenshot of GIM homepage' width='300' />
-                <p>tech used</p>
-                <p>one line description</p>
-                <button onClick={() => setPage('GIM')}>details</button>
+            <div className={styles.container}>
+                <h1 className={styles.projectTitle}>GIM LLC.</h1>
+                <figure className={styles.projectImage}>
+                    <img src='/GIM.png' alt='screenshot of GIM homepage' width='300' />
+                </figure >
+                <div className={styles.projectInfo}>
+                    <p>GIM LLC is an escape room inspired website where players work together to find clues to free the ghost!</p>
+                    <p>React | Socket.io<br />Context Api | Node | Express</p>
+                    <div className={styles.icons}>
+                        <a href='https://github.com/GIM-LLC' target='_blank'>
+                            <img
+                                className={styles.icon}
+                                src='/git.svg'
+                                alt='github' />
+                        </a>
+                        <a href='https://www.escapegim.com/' target='_blank'>
+                            <img
+                                className={styles.icon}
+                                src='/site.svg'
+                                alt='live site' />
+                        </a>
+                    </div>
+                    <button onClick={() => setPage('GIM')}>MORE INFO
+                    </button>
+                </div>
             </div>
-            <div>
-                <h1>GIM LLC.</h1>
-                <img src='/GIM.png' alt='screenshot of GIM homepage' width='300' />
-                <p>tech used</p>
-                <p>one line description</p>
-                <button onClick={() => setPage('GIM')}>details</button>
-            </div>
-            <div>
-                <h1>GIM LLC.</h1>
-                <img src='/GIM.png' alt='screenshot of GIM homepage' width='300' />
-                <p>tech used</p>
-                <p>one line description</p>
-                <button onClick={() => setPage('GIM')}>details</button>
-            </div>
-            <div>
-                <h1>GIM LLC.</h1>
-                <img src='/GIM.png' alt='screenshot of GIM homepage' width='300' />
-                <p>tech used</p>
-                <p>one line description</p>
-                <button onClick={() => setPage('GIM')}>details</button>
-            </div>
-
         </div>
     )
 };
