0f80da83cea9c796f35c16d24c890683d64bad30
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-07-27 21:53:16 -0700

    tidying up for first release

--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
-// link to contact me email form
-// https://medium.com/nerd-for-tech/coding-a-contact-form-with-next-js-and-nodemailer-d3a8dc6cd645
