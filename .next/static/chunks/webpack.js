fcb9c4dfe54ed831a407c9da387a13168caad453
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-07-30 14:53:49 -0700

    styling fixes from merge issues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "227f48638a469fdba8c7"; }
+/******/ 		__webpack_require__.h = function() { return "8a026c3515956f8a5204"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
