d6ad2cc288d586076655c11b0f67eccf0799ac72
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-07-30 13:51:54 -0700

    merge conflict fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "716c00636f9e70d9322d"; }
+/******/ 		__webpack_require__.h = function() { return "9225b546324cb3754346"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
