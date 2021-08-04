30185dc8c809d20c9ed7f4cb0ee5d76d3b839d88
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-08-04 16:11:33 -0700

    update header info on page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a026c3515956f8a5204"; }
+/******/ 		__webpack_require__.h = function() { return "9613e3cfe62eff0eff81"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
