7269fc6da150a1f1cd311598f63f7b1de11e9aaa
KatrinaCloyd <katrina.cloyd@yahoo.com>
2023-01-04 17:07:14 -0800

    add readme and some placeholder info

--- components/navigation.js
+++ components/navigation.js
@@ -1,24 +1,24 @@
 import Link from "next/link";
 import styles from "../styles/Nav.module.css";
 
-export default function Navigation() {
-  return (
-    <nav className={styles.navList}>
-      <Link href="/">
-        <a className={styles.navItem}>HOME</a>
-      </Link>
-      <Link href="/about">
-        <a className={styles.navItem}>ABOUT</a>
-      </Link>
-      <Link href="/dev">
-        <a className={styles.navItem}>DEV WORK</a>
-      </Link>
-      <Link href="/design">
-        <a className={styles.navItem}>DESIGN WORK</a>
-      </Link>
-      <Link href="/contact">
-        <a className={styles.navItem}>CONTACT</a>
-      </Link>
-    </nav>
-  );
-}
+export default function Navigation(home) {
+    return (
+        <nav className={styles.navList}>
+            <Link href='/'>
+                <a className={home ? [styles.selected, styles.navItem].join(' ') : styles.navItem}>HOME</a>
+            </Link>
+            <Link href='/about'>
+                <a className={styles.navItem}>ABOUT</a>
+            </Link>
+            <Link href='/dev'>
+                <a className={styles.navItem}>DEV WORK</a>
+            </Link>
+            <Link href='/design'>
+                <a className={styles.navItem}>DESIGN WORK</a>
+            </Link>
+            <Link href='/contact'>
+                <a className={styles.navItem}>CONTACT</a>
+            </Link>
+        </nav>
+    )
+};
