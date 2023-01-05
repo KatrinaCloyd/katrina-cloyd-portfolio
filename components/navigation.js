fbc6a8fa286be3eb7c19cc619503efa79251c8de
KatrinaCloyd <katrina.cloyd@yahoo.com>
2023-01-04 17:15:47 -0800

    added Link routing to nav component, changed all page components to pages and updated the index to conatin main layout

--- components/navigation.js
+++ components/navigation.js
@@ -1,28 +1,37 @@
+import Link from 'next/link';
+import { useRouter } from "next/router";
 import styles from '../styles/Nav.module.css'
 
-export default function Navigation({ page, setPage }) {
+export default function Navigation() {
+    const router = useRouter();
 
     return (
         <nav className={styles.navList}>
-            <a className={page === 'home' ? [styles.selected, styles.navItem].join(' ') : styles.navItem}
-                onClick={() => { setPage('home') }}>
-                HOME</a>
-            <a className={page === 'about' ? [styles.selected, styles.navItem].join(' ') : styles.navItem}
-                onClick={() => { setPage('about') }}>
-                ABOUT
-            </a>
-            <a className={page === 'dev' ? [styles.selected, styles.navItem].join(' ') : styles.navItem}
-                onClick={() => { setPage('dev') }}>
-                DEV WORK
-            </a>
-            <a className={page === 'design' ? [styles.selected, styles.navItem].join(' ') : styles.navItem}
-                onClick={() => { setPage('design') }}>
-                DESIGN WORK
-            </a>
-            <a className={page === 'contact' ? [styles.selected, styles.navItem].join(' ') : styles.navItem}
-                onClick={() => { setPage('contact') }}>
-                CONTACT
-            </a>
+            <Link href='/'>
+                <div className={router.asPath === '/' ? [styles.selected, styles.navItem].join(' ') : styles.navItem} >
+                    HOME
+                </div>
+            </Link>
+            <Link href='/About'>
+                <div className={router.asPath === '/About' ? [styles.selected, styles.navItem].join(' ') : styles.navItem} >
+                    ABOUT
+                </div>
+            </Link>
+            <Link href='/DevWork'>
+                <div className={router.asPath === '/DevWork' ? [styles.selected, styles.navItem].join(' ') : styles.navItem} >
+                    DEV WORK
+                </div>
+            </Link>
+            <Link href='/DesignWork'>
+                <div className={router.asPath === '/DesignWork' ? [styles.selected, styles.navItem].join(' ') : styles.navItem} >
+                    DESIGN WORK
+                </div>
+            </Link>
+            <Link href='/Contact'>
+                <div className={router.asPath === '/Contact' ? [styles.selected, styles.navItem].join(' ') : styles.navItem} >
+                    CONTACT
+                </div>
+            </Link>
         </nav>
     )
 };
