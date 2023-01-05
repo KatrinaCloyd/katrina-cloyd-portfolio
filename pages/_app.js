d20708303ec21f7f83494ae3148f357e585c840d
KatrinaCloyd <katrina.cloyd@yahoo.com>
2023-01-04 17:05:50 -0800

    inital commit, set up using nextjs bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,7 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Navigation from '../components/navigation'
-import '../styles/globals.css'
-import styles from '../styles/Home.module.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-
-  return (
-    <>
-      <Head>
-        <title>Katrina Cloyd Portfolio Site</title>
-        <meta name="description" content="software engineer && graphic design portfolio" />
-      </Head>
-      <div className={styles.container}>
-        <Header />
-        <section className={styles.page}>
-          <main className={styles.grid}>
-            <Component {...pageProps} />
-          </main>
-          <Navigation />
-        </section>
-      </div>
-    </>
-  )
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
